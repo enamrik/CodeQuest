a5489ae6259a963f6005c8055619dd4a4c0b1a22
Kirmanie L Ravariere <enamrik@gmail.com>
2014-12-23 14:16:06 -0500

    Started move to ruby for scripting

---
+++ scripts/base_image.rb
@@ -0,0 +1,16 @@
+
+class BaseImage
+    attr_accessor :docker
+
+    def initialize(docker)
+        @docker = docker
+    end
+    def build_if_missing
+
+        unless docker.image_exists? 'codequest_base'
+            puts 'codequest_base image hasn\'t been built yet. Building...'
+            context_path = File.expand_path("#{File.dirname(__FILE__)}/../base_image")
+            docker.build("-t codequest_base #{context_path}")
+        end
+    end
+end
---
+++ scripts/docker.rb
@@ -0,0 +1,74 @@
+class Docker
+    def setup_with(options)
+        setup_boot2docker_ports options[:ports]
+        setup_docker_environment
+    end
+
+    def build(params)
+        exec("build #{params}", stream_out: true)
+    end
+
+    def run(params)
+        exec("run #{params}", stream_out: true)
+    end
+
+    def image_exists?(image_name)
+        exec('images').include? image_name
+    end
+
+    def container_exists?(container_name)
+        exec('ps -a').include? container_name
+    end
+
+    def exec(command, options = {})
+        cmd = "docker #{command}"
+        puts "DOCKER: #{cmd}"
+        Command.run(cmd, options)
+    end
+
+    @private
+
+    def setup_docker_environment
+        puts "Setting up docker environment variables..."
+        Command.run('boot2docker shellinit')
+            .split("\n")
+            .select{|line| line.include?'export'}
+            .each {|line|
+                env_parts = line.sub('export','').split('=')
+                ENV[env_parts.first.strip] = env_parts[1]
+            }
+    end
+
+    def setup_boot2docker_ports(ports_needed)
+        ports_to_expose = ports_needed.reject {|port|
+            port_forward_entry_exists_for port
+        }
+
+        if ports_to_expose.any?
+            puts "Could not find port forward entries #{}. Preparing to add..."
+
+            if boot2docker_running?
+                puts 'Shutting down VM...'
+                Command.run('boot2docker down')
+            end
+
+            ports_to_expose.each do|port|
+                puts "Adding port forward entry #{port}..."
+                Command.run("VBoxManage modifyvm \"boot2docker-vm\" --natpf1 \"tcp-port#{port},tcp,,#{port},,#{port}\"")
+            end
+        end
+
+        unless boot2docker_running?
+            puts 'boot2docker VM not running. Booting VM...'
+            Command.run('boot2docker up')
+        end
+    end
+
+    def port_forward_entry_exists_for(port)
+        Command.run('VBoxManage showvminfo "boot2docker-vm"').include? "tcp-port#{port}"
+    end
+
+    def boot2docker_running?
+        Command.run('boot2docker status').include? 'running'
+    end
+end
